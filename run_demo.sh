e859614c60decc68a566d2c07198ca8d815a2662
Kushal Kedia <kushal.k.2000@gmail.com>
2023-10-26 16:55:55 -0400

    modified run code

--- run_demo.sh
+++ run_demo.sh
@@ -1,4 +1,14 @@
 # python demo.py --image_path demo_images/tanager.jpg --text_prompt back,beak,belly,breast,crown,forehead,eyes,legs,wings,nape,tail,throat -o outputs/ --device cuda --get_topk
 # python demo.py --image_path table_items.png --text_prompt="white salt bottle,red pepper bottle,pot,ladle" -o outputs/ --device cuda
 # python demo.py --image_path items_right_camera.jpg --text_prompt="ketchup,mustard,pot,ladle" -o outputs/ --device cuda --get_topk
-python realtime_OD.py --video_path table_left.MOV --text_prompt="ketchup,blue tartar bottle,black pot,ladle" -o video_outputs_left/ --device cuda --get_topk --view=right
+python realtime_OD.py --video_path table_right.MOV --text_prompt="ketchup,tartar bottle,pot,ladle" \
+-o video_outputs_left/ --device cuda --get_topk --view=right
+
+python realtime_OD.py --video_path table_left.MOV --text_prompt="ketchup,tartar bottle,pot,ladle" \
+-o video_outputs_left/ --device cuda --get_topk --view=left
+
+python realtime_OD.py --video_path table_right.MOV --text_prompt="ketchup,blue tartar bottle,black pot,ladle" \
+-o video_outputs_left/ --device cuda --get_topk --view=right
+
+python realtime_OD.py --video_path table_left.MOV --text_prompt="ketchup,blue tartar bottle,black pot,ladle" \
+-o video_outputs_left/ --device cuda --get_topk --view=left
